1630adc2dc70d0e87fea37d68146f9d1219c3b1c
Neprokin <Rexano@yandex.ru>
2026-02-27 22:50:00 +0300

    1.3

--- Contest 1 - bin_search, sorts/Задание 3 - Отгадай число.py
+++ Contest 1 - bin_search, sorts/Задание 3 - Отгадай число.py
@@ -1,21 +1,20 @@
 import sys
 
 def query(x):
-    print(x)
+    print(x, flush=True)
     sys.stdout.flush()
     return input()
 
 
 n = int(input())
 
-flag = False
-r = n - 1
-l = 0
-
-while r - l > 1:
-    m = int((l + r) // 2)
+r = n
+l = 1
+m = 0
+while (r - l > 1):
+    m = int((l + r) / 2)
     if query(m) == '>=':
         l = m
     else:
         r = m
-print(f"{(l + r) / 2:.6f}")
+print(f"!{m}")
